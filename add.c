7883d2a7dfc53c9c0c2246d120db6636c6267459
lingasw <54500358+lingasw@users.noreply.github.com>
2020-05-07 12:08:17 +0530

    Update add.c

--- add.c
+++ add.c
@@ -4,6 +4,8 @@ void main()
 int a=2,b=3,c;
   c=a+b;
   c=a-b;
+  c=a*b;
 printf("add of two numbers are %d",c);
+  printf("add of two numbers are %d",c);
   printf("add of two numbers are %d",c);
 }
